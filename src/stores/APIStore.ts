cb550f72a2384689eb3af5824d994b11ac017f19
zpzhou <himoker@163.com>
2023-07-09 14:06:42 +0800

    update prompt

--- src/stores/APIStore.ts
+++ src/stores/APIStore.ts
@@ -4,7 +4,7 @@ export const useAPIStore = defineStore('api', {
   state: () => ({
     modelOptions: ['gpt-3.5-turbo', 'gpt-4'],
     model: 'gpt-3.5-turbo',
-    temperatureOptions: [2, 1.9, 1.8, 1.7, 1.6, 1.5, 1.4, 1.3, 1.2, 1.1, 1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1],
+    temperatureOptions: [2, 1.9, 1.8, 1.7, 1.6, 1.5, 1.4, 1.3, 1.2, 1.1, 1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0],
     temperature: 0.7
   }),
 
