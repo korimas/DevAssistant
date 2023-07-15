30367c61bf16ab070c9009f1505d2ec5ea048bbd
zpzhou <himoker@163.com>
2023-07-15 14:03:27 +0800

    update prompt

--- src/pages/Requirement.vue
+++ src/pages/Requirement.vue
@@ -69,7 +69,9 @@
                 >
                   <template v-slot:after>
                     <div class="column">
-                      <q-btn dense flat icon="save" @click="ParseDetailMarkdown"/>
+                      <q-btn dense flat icon="save" @click="ParseDetailMarkdown">
+                        <q-tooltip class="bg-grey">保存并重新生成软件需求</q-tooltip>
+                      </q-btn>
                       <q-btn dense flat color="primary" @click="inEditDetail = false">取消</q-btn>
                     </div>
                   </template>
