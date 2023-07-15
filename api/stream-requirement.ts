462f22b2f9e6e882d7e3d7b400fd8eb1536b6fc5
zpzhou <himoker@163.com>
2023-07-15 15:55:56 +0800

    update prompt

--- api/stream-requirement.ts
+++ api/stream-requirement.ts
@@ -165,7 +165,7 @@ const handler = async (req: Request): Promise<Response> => {
 
     prompt = prompt + `
 
-#99 输出的结果必须以markdown table展示并且只输出这个table，table必须遵循以下格式：
+#99 输出的结果必须以markdown table展示并且只输出这个table，table必须遵循以下格式，编号从1开始：
 """
 | 编号 | 标题 | 描述 | 验证准则 |
 | ---- | --- | ---- | ------ |
