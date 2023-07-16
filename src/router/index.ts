56bd7f8712242e0deeb981c4ba3b7c828594bef4
zpzhou <himoker@163.com>
2023-07-16 19:56:15 +0800

    update style

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,8 @@
-import { route } from 'quasar/wrappers';
-import {
-  createMemoryHistory,
-  createRouter,
-  createWebHashHistory,
-  createWebHistory,
-} from 'vue-router';
+import {route} from 'quasar/wrappers';
+import {createMemoryHistory, createRouter, createWebHashHistory, createWebHistory,} from 'vue-router';
 
 import routes from './routes';
+import {Loading} from 'quasar'
 
 /*
  * If not building with SSR mode, you can
@@ -23,7 +19,7 @@ export default route(function (/* { store, ssrContext } */) {
     : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory);
 
   const Router = createRouter({
-    scrollBehavior: () => ({ left: 0, top: 0 }),
+    scrollBehavior: () => ({left: 0, top: 0}),
     routes,
 
     // Leave this as is and make changes in quasar.conf.js instead!
@@ -32,5 +28,15 @@ export default route(function (/* { store, ssrContext } */) {
     history: createHistory(process.env.VUE_ROUTER_BASE),
   });
 
+  Router.beforeEach((to, from, next) => {
+    // 在路由切换前显示加载动画
+    Loading.show();
+    next();
+  });
+
+  Router.afterEach(() => {
+    // 在路由切换后隐藏加载动画
+    Loading.hide();
+  });
   return Router;
 });
