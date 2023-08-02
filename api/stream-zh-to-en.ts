beb6d6c3ddab83a3b4c48023287c1eb6b03895ca
zpzhou <himoker@163.com>
2023-08-02 19:53:48 +0800

    update prompot

--- api/stream-zh-to-en.ts
+++ api/stream-zh-to-en.ts
@@ -13,7 +13,7 @@ const handler = async (req: Request): Promise<Response> => {
   const recvPayload = await req.json()
   const isChinese = hasChinese(recvPayload.requirement)
   let prompt = `
-    #01 我想让你充当翻译员，你可以在不改变原意的前提下对它进行美化和修正。
+    #01 我想让你充当翻译员，你可以在不改变原意的前提下对内容进行美化和修正。
     #02 你只需要翻译输入的内容，不必对内容中的问题或要求进行解释或回答，只需要翻译它。
     `
 
