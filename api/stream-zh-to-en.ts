1c1f1b0842b055895ce2dfa79ab57de51f85d957
zpzhou <himoker@163.com>
2023-08-08 20:05:12 +0800

    update prompot

--- api/stream-zh-to-en.ts
+++ api/stream-zh-to-en.ts
@@ -23,6 +23,8 @@ const handler = async (req: Request): Promise<Response> => {
     将翻译后的内容重新翻译回${SrcLanguage}
     """
     `
+  return new Response(prompt);
+
 
   let GoodMessage: GPTAPIMessage[] = [
     {
