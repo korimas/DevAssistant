d0bd5fe9d4db265905bbd0c5df1d347f7edb7207
Korimas <himoker@163.com>
2024-08-31 22:06:05 +0800

    update

--- src/stores/APIStore.ts
+++ src/stores/APIStore.ts
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia';
 export const useAPIStore = defineStore('api', {
   state: () => ({
     modelOptions: ['gpt-4-turbo', 'gpt-4', 'gpt-4o', 'gpt-4o-mini', 'gpt-3.5-turbo'],
-    model: 'gpt-3.5-turbo',
+    model: 'gpt-4o-mini',
     temperatureOptions: Array.from({ length: 21 }, (_, index) => 2.0 - (index * 0.1)),
     temperature: 1
   })
