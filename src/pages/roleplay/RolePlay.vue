be8bb8bfbbe2db54b1a7cc926b224a8a1e6ce270
korimas <himoker@163.com>
2025-02-09 18:13:59 +0800

    update

--- src/pages/roleplay/RolePlay.vue
+++ src/pages/roleplay/RolePlay.vue
@@ -1,32 +1,115 @@
 <template>
   <q-page class="row">
-    <ChatDialog :InputSystemPrompt="InputSystemPrompt" :max-number="rolePlayPrompt.maxNum" fource-model="gpt-3.5-turbo"
-      @update-system-prompt="setSystemPrompt" @update-new-chat="handleNewChat">
+    <ChatDialog
+      :InputSystemPrompt="InputSystemPrompt"
+      :max-number="rolePlayPrompt.maxNum"
+      @update-system-prompt="setSystemPrompt"
+      @update-new-chat="handleNewChat"
+    >
       <template v-slot:setting-drawer>
-        <q-input dense clearable autogrow label="回顾区大小" v-model="maxReviewItemsNum" outlined class="full-width"
-          style="margin-bottom: 10px" />
-        <q-input dense clearable autogrow label="人物名称" v-model="rolePlayPrompt.roleName" outlined class="full-width"
-          style="margin-bottom: 10px" @update:model-value="RolePlayPromptUpdate" />
-        <q-input dense clearable autogrow label="我的名称" v-model="rolePlayPrompt.myName" outlined class="full-width"
-          style="margin-bottom: 10px" @update:model-value="RolePlayPromptUpdate" />
-        <q-input dense clearable autogrow label="规则区" v-model="rolePlayPrompt.rulesArea" outlined class="full-width"
-          style="margin-bottom: 10px" @update:model-value="RolePlayPromptUpdate" />
-        <q-input dense autogrow clearable label="记忆区" v-model="rolePlayPrompt.memoryArea" outlined class="full-width"
-          style="margin-bottom: 10px" @update:model-value="RolePlayPromptUpdate" />
-        <q-input v-for="(item, index) in rolePlayPrompt.reviewItems" :key="index" dense clearable autogrow
-          :label="'回顾区' + (index + 1)" v-model="rolePlayPrompt.reviewItems[index]" outlined class="full-width"
-          style="margin-bottom: 10px" @clear="clearReviewItem(index)" @update:model-value="RolePlayPromptUpdate" />
+        <q-input
+          dense
+          clearable
+          autogrow
+          label="回顾区大小"
+          v-model="maxReviewItemsNum"
+          outlined
+          class="full-width"
+          style="margin-bottom: 10px"
+        />
+        <q-input
+          dense
+          clearable
+          autogrow
+          label="人物名称"
+          v-model="rolePlayPrompt.roleName"
+          outlined
+          class="full-width"
+          style="margin-bottom: 10px"
+          @update:model-value="RolePlayPromptUpdate"
+        />
+        <q-input
+          dense
+          clearable
+          autogrow
+          label="我的名称"
+          v-model="rolePlayPrompt.myName"
+          outlined
+          class="full-width"
+          style="margin-bottom: 10px"
+          @update:model-value="RolePlayPromptUpdate"
+        />
+        <q-input
+          dense
+          clearable
+          autogrow
+          label="规则区"
+          v-model="rolePlayPrompt.rulesArea"
+          outlined
+          class="full-width"
+          style="margin-bottom: 10px"
+          @update:model-value="RolePlayPromptUpdate"
+        />
+        <q-input
+          dense
+          autogrow
+          clearable
+          label="记忆区"
+          v-model="rolePlayPrompt.memoryArea"
+          outlined
+          class="full-width"
+          style="margin-bottom: 10px"
+          @update:model-value="RolePlayPromptUpdate"
+        />
+        <q-input
+          v-for="(item, index) in rolePlayPrompt.reviewItems"
+          :key="index"
+          dense
+          clearable
+          autogrow
+          :label="'回顾区' + (index + 1)"
+          v-model="rolePlayPrompt.reviewItems[index]"
+          outlined
+          class="full-width"
+          style="margin-bottom: 10px"
+          @clear="clearReviewItem(index)"
+          @update:model-value="RolePlayPromptUpdate"
+        />
         <div class="row">
-          <q-checkbox left-label v-model="rolePlayPrompt.rolePlayConfig.enableRoleState" label="开启角色状态(Beta)"
-            checked-icon="task_alt" unchecked-icon="highlight_off" @update:model-value="RolePlayPromptUpdate" />
-          <q-input v-if="rolePlayPrompt.rolePlayConfig.enableRoleState" dense clearable autogrow label="角色状态"
-            v-model="rolePlayPrompt.roleState" outlined class="full-width" style="margin-bottom: 10px"
-            @update:model-value="RolePlayPromptUpdate" />
+          <q-checkbox
+            left-label
+            v-model="rolePlayPrompt.rolePlayConfig.enableRoleState"
+            label="开启角色状态(Beta)"
+            checked-icon="task_alt"
+            unchecked-icon="highlight_off"
+            @update:model-value="RolePlayPromptUpdate"
+          />
+          <q-input
+            v-if="rolePlayPrompt.rolePlayConfig.enableRoleState"
+            dense
+            clearable
+            autogrow
+            label="角色状态"
+            v-model="rolePlayPrompt.roleState"
+            outlined
+            class="full-width"
+            style="margin-bottom: 10px"
+            @update:model-value="RolePlayPromptUpdate"
+          />
         </div>
 
-        <q-input dense clearable autogrow label="示例输出" v-model="rolePlayPrompt.exampleOutput" outlined
-          class="full-width" style="margin-bottom: 10px" @update:model-value="RolePlayPromptUpdate" />
-        <span style="white-space: pre-wrap;"> {{ InputSystemPrompt }}</span>
+        <q-input
+          dense
+          clearable
+          autogrow
+          label="示例输出"
+          v-model="rolePlayPrompt.exampleOutput"
+          outlined
+          class="full-width"
+          style="margin-bottom: 10px"
+          @update:model-value="RolePlayPromptUpdate"
+        />
+        <span style="white-space: pre-wrap"> {{ InputSystemPrompt }}</span>
       </template>
     </ChatDialog>
   </q-page>
@@ -80,7 +163,8 @@ function handleNewChat(inputContent: string, outputContent: string) {
   }
 
   rolePlayPrompt.value.reviewItems.push(
-    `${rolePlayPrompt.value.myName}：${inputContent}\n${rolePlayPrompt.value.roleName
+    `${rolePlayPrompt.value.myName}：${inputContent}\n${
+      rolePlayPrompt.value.roleName
     }：${outputContent.trimEnd()}`
   );
   if (maxReviewItemsNum.value <= 0) {
